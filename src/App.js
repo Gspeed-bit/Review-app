734d6c658ee722ae9c3be0a18ff9ff89f60b6ad9
Your Name <adeyeye17@gmail.com>
2021-11-08 20:45:30 +0100

    prevButton and nextButton was implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import React, {useState} from "react"
 import reviewsList from "./data"
 import Reviews from "./Reviews"
 import './App.css';
+import reviews from "./data";
 
 function App() {
 
-  const [person, setPerson] = Use
+  const [person, setPerson] = useState(0)
+  const {...review} = reviewsList[person]
 
- return (
+
+
+  const prevPerson =() =>{
+    setPerson((person)=>{
+      let newPerson = person - 1
+      return newPerson
+    })
+  }
+  const nextPerson =() =>{
+    setPerson((person)=>{
+      let newPerson = person + 1
+      return newPerson
+    })
+  }
+
+ 
+
+  return (
     <div className="App">
-      <h1> Our Reviews</h1>
+      <h1> Our Reviews </h1>
       <div>
-        
+        <Reviews  nextPerson={nextPerson} prevPerson={prevPerson} review={review} />
       </div>
  
     </div>
--- src/Reviews.js
+++ src/Reviews.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight} from 'react-icons/fa';
+ const Reviews = ({review, prevPerson, nextPerson, randomPerson}) => {
 
- const Reviews = () => {
+
     return (
-       <h1> ihjakdash</h1>
+        <div>
+            <h4> {review.name} </h4>
+            <button onClick={prevPerson}> <FaArrowAltCircleLeft /> </button>
+            <button onClick={nextPerson}> <FaArrowAltCircleRight /> </button>
+            <button onClick={randomPerson}> jhgkfs </button>
+
+
+        </div>
+      
     )
 }
 export default Reviews
