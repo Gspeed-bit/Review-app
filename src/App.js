f94d3bf9c56c36b24a4fce25c2c2b38a8303195b
Your Name <adeyeye17@gmail.com>
2021-11-08 21:05:55 +0100

    checkNumber function# was implemented

--- src/App.js
+++ src/App.js
@@ -7,20 +7,33 @@ import reviews from "./data";
 function App() {
 
   const [person, setPerson] = useState(0)
-  const {...review} = reviewsList[person]
+  const {...review} = reviewsList[person] // Spread operator was used to get the content in the external data
+
+
+  const checkNumber = (number)=>{
+      if(number > reviewsList.length-1){
+        return 0
+      }
+      if(number < 0){
+        return reviewsList.length -1
+      }
+      return number
+     }
+  
 
 
 
+  // functionality for the prevPerson
   const prevPerson =() =>{
     setPerson((person)=>{
       let newPerson = person - 1
-      return newPerson
+      return checkNumber(newPerson)
     })
   }
   const nextPerson =() =>{
     setPerson((person)=>{
       let newPerson = person + 1
-      return newPerson
+      return checkNumber(newPerson)
     })
   }
 
@@ -30,7 +43,7 @@ function App() {
     <div className="App">
       <h1> Our Reviews </h1>
       <div>
-        <Reviews  nextPerson={nextPerson} prevPerson={prevPerson} review={review} />
+        <Reviews   randomPerson ={randomPerson} nextPerson={nextPerson} prevPerson={prevPerson} review={review} />
       </div>
  
     </div>
