0f3e0e2a3f1fb811bb774f1a03cdda65f1c9d27b
Your Name <adeyeye17@gmail.com>
2021-11-08 20:27:24 +0100

    first commit for the review component

---
+++ src/Reviews.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const Reviews = () => {
+    return (
+       <h1> ihjakdash</h1>
+    )
+}
+export default Reviews
+
---
+++ src/data.js
@@ -0,0 +1,76 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    text:
+      "The test of the machine is the satisfaction it gives you. There isn't any other test. If the machine produces tranquility it's right. If it disturbs you it's wrong until either the machine or your mind is changed.",
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://images.pexels.com/photos/10132965/pexels-photo-10132965.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    text:
+    "We must not blame programmers for their bugs. They belong to them only until the code is merged to the repository. After that, all bugs are ours!",
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'software engineer',
+    image:
+      'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    text:
+      'When one of the workers suggested they could automate some of the manager’s job with software they could easily develop, they were instructed not to do so.',
+  },
+  {
+    id: 4,
+    name: 'Peter Noah',
+    job: 'the boss',
+    image:
+      'https://images.pexels.com/photos/10131163/pexels-photo-10131163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    text:
+      'Any software project must have a technical leader, who is responsible for all technical decisions made by the team and have enough authority to make them. Responsibility and authority are two mandatory components that must be present in order to make it possible to call such a person an architect.',
+  },
+   {
+    id: 5,
+    name: 'Julia Anderson',
+    job: 'HR manager ',
+    image:
+      'https://images.pexels.com/photos/1024311/pexels-photo-1024311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    text:
+      'When one of the workers suggested they could automate some of the manager’s job with software they could easily develop, they were instructed not to do so.',
+  },
+   {
+    id: 6,
+    name: 'Katharina Kurt',
+    job: 'ui/ux Designer ',
+    image:
+      'https://images.pexels.com/photos/1082962/pexels-photo-1082962.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    text:
+    "Unintentional design happens when the organization focuses on the business or the technology, but doesn’t focus on the customer or user experience. The result is an experience that emerges that often isn’t pleasant.",
+  },
+   {
+    id: 7,
+    name: 'Monika Walter ',
+    job: 'intern',
+    image:
+      'https://images.pexels.com/photos/8219320/pexels-photo-8219320.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    text:
+      "Software is easy to make, except when you want it to do something new. And then, of course, there is a corollary: The only software that's worth making is software that does something new.",
+  },
+   {
+    id: 8,
+    name: 'Stefan Alexander',
+    job: 'Database administrator',
+    image:
+      'https://images.pexels.com/photos/1121796/pexels-photo-1121796.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    text:
+      'The job of a tester is to prove that the software is bug free, while it has to be the other way around: The job of a tester is to prove that the software is broken. The better testers are doing their jobs, the more bugs they manage to find and report. ',
+  },
+];
+
+export default reviews;
